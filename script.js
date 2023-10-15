8e9d2e71e20bd3d34942c92ab319263cb0fd7840
f-r-b <132161012+f-r-b@users.noreply.github.com>
2023-10-15 19:41:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -67,6 +67,61 @@ playButton.addEventListener("click", () => {
     alert('Le mot contient des caractères non autorisés.');
 } 
 }); */
+function startSolitaireMode() {
+    const { word, hint } = generateWordAndHint();
+
+    const wordDisplay = document.getElementById("wordDisplay");
+    const gameResult = document.getElementById("gameResult");
+
+    
+    alert(`Indice : ${hint}`);
+
+    wordDisplay.textContent = "_ ".repeat(word.length);
+
+    const letters = document.querySelectorAll(".letter");
+    let incorrectAttempts = 0;
+    const maxAttempts = word.length * 2;
+
+    letters.forEach((letter) => {
+        letter.addEventListener("click", () => {
+            if (incorrectAttempts >= maxAttempts) {
+                return;
+            }
+            const guessedLetter = letter.textContent;
+            if (word.includes(guessedLetter)) {
+                letter.classList.add("correct");
+            } else {
+                letter.classList.add("incorrect");
+                incorrectAttempts++;
+            }
+
+            const wordArray = wordDisplay.textContent.split(" ");
+            word.split("").forEach((char, index) => {
+                if (char === guessedLetter) {
+                    wordArray[index] = char;
+                }
+            });
+            wordDisplay.textContent = wordArray.join(" ");
+
+            if (wordDisplay.textContent.indexOf("_") === -1) {
+                gameResult.textContent = "Gagné!";
+            } else if (incorrectAttempts >= maxAttempts) {
+                gameResult.textContent = `Perdu! Le mot était : ${word}`;
+            }
+        });
+    });
+}
+};
+
+function generateWordAndHint() {
+    const wordsAndHints = [
+        { word: "PENDU", hint: "Un jeu de devinette" },
+        { word: "INFORMATIQUE", hint: "Le domaine des ordinateurs" },
+    ];
+
+    const randomIndex = Math.floor(Math.random() * wordsAndHints.length);
+    return wordsAndHints[randomIndex];
+}
 function startMultiplayerMode() {
     const userInput = prompt("Entrez le mot à deviner:");
 
