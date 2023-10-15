44de9da132c1f8acaf8c1789fb562400cda64ddc
f-r-b <132161012+f-r-b@users.noreply.github.com>
2023-10-15 18:36:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ multiplayerButton.addEventListener("click", () => {
 playButton.addEventListener("click", () => {
     playButton.style.display = "none"; 
     game.style.display = "block";
-    
+} /*  
     const userInput = prompt("Entrez le mot à deviner:");
 
     if (/^[a-zA-Z\s]*$/.test(userInput)) {
@@ -66,4 +66,51 @@ playButton.addEventListener("click", () => {
 } else {
     alert('Le mot contient des caractères non autorisés.');
 } 
-}); 
+}); */
+function startMultiplayerMode() {
+    const userInput = prompt("Entrez le mot à deviner:");
+
+    if (/^[a-zA-Z\s]*$/.test(userInput)) {
+        const wordDisplay = document.getElementById("wordDisplay");
+        const gameResult = document.getElementById("gameResult");
+        const word = userInput ? userInput.toUpperCase() : "";
+        wordDisplay.textContent = "_ ".repeat(word.length);
+
+        const letters = document.querySelectorAll(".letter");
+        let incorrectAttempts = 0;
+        const maxAttempts = word.length * 2;
+
+        letters.forEach((letter) => {
+            letter.addEventListener("click", () => {
+                if (incorrectAttempts >= maxAttempts) {
+                    return;
+                }
+                const guessedLetter = letter.textContent;
+                if (word.includes(guessedLetter)) {
+                    letter.classList.add("correct");
+                } else {
+                    letter.classList add("incorrect");
+                    incorrectAttempts++;
+                }
+
+                const wordArray = wordDisplay.textContent.split(" ");
+                word.split("").forEach((char, index) => {
+                    if (char === guessedLetter) {
+                        wordArray[index] = char;
+                    }
+                });
+                wordDisplay.textContent = wordArray.join(" ");
+
+                if (wordDisplay.textContent.indexOf("_") === -1) {
+                    gameResult.textContent = "Gagné!";
+                } else if (incorrectAttempts >= maxAttempts) {
+                    gameResult.textContent = `Perdu! Le mot était : ${word}`;
+                }
+            });
+        });
+    } else if (userInput.length === 0 || userInput.includes(' ') || userInput.indexOf(' ') !== -1) {
+        alert('Le mot contient un espace ou est vide !');
+    } else {
+        alert('Le mot contient des caractères non autorisés.');
+    }
+}
